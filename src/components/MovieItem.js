9984254dd79648efc985866c146150bcd9dd1c3c
Jemma <72512609+JemmaW7@users.noreply.github.com>
2024-10-13 22:43:02 +0100

    comments added

--- src/components/MovieItem.js
+++ src/components/MovieItem.js
@@ -3,6 +3,7 @@ import Card from 'react-bootstrap/Card';
 
 function MoviesItem(props){
 
+    // useEffect hook to log the movie details when the myMovie prop changes
     useEffect(() => {
         console.log("Movie Item:", props.myMovie);
     }, [props.mymovie]); // Only run this effect when the mymovie prop changes
@@ -10,10 +11,13 @@ function MoviesItem(props){
     return (
         <div>
         <Card>
+           {/* card header displays the movie title */}
           <Card.Header>{props.myMovie.Title}</Card.Header>
           <Card.Body>
+            {/* blockquote contains the movie poster and year */}
             <blockquote className="blockquote mb-0">
               <img src={props.myMovie.Poster} alt={props.myMovie.Title} />
+              {/* movie release year */}
               <footer>{props.myMovie.Year}</footer>
             </blockquote>
           </Card.Body>
